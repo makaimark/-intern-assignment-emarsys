2403cae662977deb8780f07b99567c3a0862bb60
makaimark <makaimark@gmail.com>
2017-03-20 18:12:38 +0100

    orderDestinations function refactored

--- src/main/java/OrderLogic.java
+++ src/main/java/OrderLogic.java
@@ -14,12 +14,11 @@ public class OrderLogic {
     public static List<String> orderDestinations(List<String[]> destinationDependencies){
         List<String> allDestinations = getAllDestination(destinationDependencies);
         for(String[] dependency : destinationDependencies){
-            System.out.println(allDestinations);
             if(!(dependency.length == 1)){
-                int index = allDestinations.indexOf(dependency[1]);
-                allDestinations.remove(index);
-                allDestinations.add(0, dependency[1]);
+                allDestinations.remove(allDestinations.indexOf(dependency[1]));
+                allDestinations.add(allDestinations.indexOf(dependency[0]), dependency[1]);
             }
+            System.out.println(allDestinations);
         }
         //System.out.println(allDestinations);
         return allDestinations;
